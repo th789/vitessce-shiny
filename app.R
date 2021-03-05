d567555663752871890eef7b3ff76ca31ef0333d
th <than@THs-MacBook-Pro-13.local>
2021-03-05 12:13:49 -0500

    basic demo

--- app.R
+++ app.R
@@ -1,7 +1,6 @@
-#load libraries
+
 library(shiny)
 library(vitessce)
-
 
 
 #load datasets
@@ -11,82 +10,93 @@ data_pbmc <- readRDS("~/Dropbox/ddesktop/lab-gehlenborg/vitessce-shiny/data/data
 data_list <- list(tcell_cd8="data_tcellcd8", pbmc="data_pbmc")
 
 
-
 #user interface
 ui <- fluidPage(
-  "Hello, world!",
+  
+  titlePanel("Vitessce demo"),
+  
+  "Select data:",
   selectInput("dataset", label="Dataset", choices=data_list),
-
+  
   "Dataset dimensions",
   verbatimTextOutput("dataset_dimensions"),
-  #tableOutput("table"),
-
+  
   "Vitessce visualization",
-  vitessce_output("vitessce_visualization"),
-
+  vitessce_output(output_id="vitessce_visualization", height="600px")
+  
 )
 
 
-
 #server
-server <- function(input, output, session) {
-  dataset <- reactive({get(input$dataset)})
-
+server <- function(input, output, session){
+  data <- reactive({get(input$dataset)})
+  
+  
   #print dimensions of dataset
   output$dataset_dimensions <- renderPrint({
-    dim(dataset())
-  })
-
-  output$vitessce_visualization <- render_vitessce({
+    dim(data())
+    })
+  
+  
+  #vitessce visualization
+  output$vitessce_visualization <- render_vitessce(expr = {
+    #create progress object
+    progress <- shiny::Progress$new()
+    progress$set(message = "", value = 0)
+    #on.exit(progress$close()) #close the progress when this reactive exits (even if there's an error)
+    #function to update progress
+    n <- 6
+    updateProgress <- function(detail = NULL) {
+      progress$inc(amount = 1/n, detail = detail)
+    }
+    
     #data processing
-    data <- dataset()
+    updateProgress("Step 1 of 3: Processing data")
+    data <- data()
     data[["percent.mt"]] <- Seurat::PercentageFeatureSet(data, pattern = "^MT-")
     data <- subset(data, subset=nFeature_RNA>200 & nFeature_RNA<2500 & percent.mt<5)
     data <- Seurat::NormalizeData(data, normalization.method="LogNormalize", scale.factor=10000)
     data <- Seurat::FindVariableFeatures(data, selection.method="vst", nfeatures=2000)
     all.genes <- rownames(data)
-    data <- Seurat::ScaleData(data, features=all.genes)
-    #PCA
-    data <- Seurat::RunPCA(data, features=Seurat::VariableFeatures(object=data))
+    
+    #data analysis
+    updateProgress("Step 2 of 3: Analyzing data (PCA)")
+    data <- Seurat::ScaleData(data, features=all.genes) #PCA
+    data <- Seurat::RunPCA(data, features=Seurat::VariableFeatures(object=data)) 
     data <- Seurat::FindNeighbors(data, dims=1:10)
     data <- Seurat::FindClusters(data, resolution=0.5)
-    #UMAP
-    data <- Seurat::RunUMAP(data, dims=1:20, min.dist=0.75)
-    #t-SNE
-    data <- Seurat::RunTSNE(data, dims=1:10, nthreads=4, max_iter=1000, k.seed=12345)
+    updateProgress("Step 2 of 3: Analyzing data (UMAP)")
+    data <- Seurat::RunUMAP(data, dims=1:20, min.dist=0.75) #UMAP
+    updateProgress("Step 2 of 3: Analyzing data (t-SNE)")
+    data <- Seurat::RunTSNE(data, dims=1:10, nthreads=4, max_iter=1000, k.seed=12345) #t-SNE
+    
+    #set up widget
+    updateProgress("Step 3 of 3: Creating Vitessce visualization")
+    vc <- VitessceConfig$new("My config")
+    dataset <- vc$add_dataset("My dataset")
+    dataset <- dataset$add_object(SeuratWrapper$new(data, cell_set_meta_names=list("seurat_clusters")))
 
-    #create Vitessce view config
-    vc <- vitessce::VitessceConfig$new("My config")
-
-    #add dataset
-    dataset <- vc$add_dataset("My dataset")$add_object(vitessce::SeuratWrapper$new(data, cell_set_meta_names=list("seurat_clusters")))
-
-    #create panels
-    #PCA
     panel_scatterplot_pca <- vc$add_view(dataset, Component$SCATTERPLOT, mapping="pca")
-    #UMAP
     panel_scatterplot_umap <- vc$add_view(dataset, Component$SCATTERPLOT, mapping="umap")
-    #t-SNE
     panel_scatterplot_tsne <- vc$add_view(dataset, Component$SCATTERPLOT, mapping="tsne")
-    #status
     panel_status <- vc$add_view(dataset, Component$STATUS)
-    #cell sets
     panel_cellsets <- vc$add_view(dataset, Component$CELL_SETS)
-
-    #layout
     vc$layout(hconcat(vconcat(panel_scatterplot_pca, panel_scatterplot_umap, panel_scatterplot_tsne),
                       vconcat(panel_cellsets, panel_status)))
-
-    #render the Vitessce widget
+    
+    vc$link_views(
+      c(panel_scatterplot_pca, panel_scatterplot_umap, panel_scatterplot_tsne),
+      c(CoordinationType$EMBEDDING_ZOOM, CoordinationType$EMBEDDING_TARGET_X, CoordinationType$EMBEDDING_TARGET_Y),
+      c_values = c(1, 0, 0)
+    )
+    
+    updateProgress("Analysis complete!")
     vc$widget()
-
+    #vc$widget(theme="light")
+    
+    
   })
-
-
+  
 }
 
-
-
 shinyApp(ui, server)
-
-
